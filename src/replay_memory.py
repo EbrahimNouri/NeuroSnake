c1049c47bd97ed9145c0b2e9915b52e7911b496c
Ebrahim Nouri <viroos@live.com>
2026-09-14 18:30:49 +0330

    replay_memory

---
+++ src/replay_memory.py
@@ -0,0 +1,39 @@
+import random
+from collections import deque
+
+import numpy as np
+
+from config import MEMORY_SIZE
+
+
+class ReplayMemory:
+
+    def __init__(self, capacity=MEMORY_SIZE):
+        self.memory = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        self.memory.append(
+            (
+                np.array(state, dtype=np.float32),
+                int(action),
+                float(reward),
+                np.array(next_state, dtype=np.float32),
+                bool(done),
+            )
+        )
+
+    def sample(self, batch_size):
+        batch = random.sample(self.memory, batch_size)
+
+        states, actions, rewards, next_states, dones = zip(*batch)
+
+        return (
+            np.array(states, dtype=np.float32),
+            np.array(actions, dtype=np.int64),
+            np.array(rewards, dtype=np.float32),
+            np.array(next_states, dtype=np.float32),
+            np.array(dones, dtype=np.float32),
+        )
+
+    def __len__(self):
+        return len(self.memory)
