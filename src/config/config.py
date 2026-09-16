56fca5c42674b5cecb55f87de9928a4023f631e7
Ebrahim Nouri <viroos@live.com>
2026-09-16 12:56:15 +0330

    Revert "LOOKAHEAD_STEPS = 12 HIDDENSIZE = 128 HiddenLayer = 2 EPSILON_END = 0.005 logger was added"
    This reverts commit dc10f357cb6509c194a741d302520d5c3cab8ba8.

--- src/config/config.py
+++ src/config/config.py
@@ -6,12 +6,12 @@ CELL_SIZE = 25
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-EPISODES = 4000
+EPISODES = 625
 
 LEARNING_RATE = 0.001
 GAMMA = 0.90
 
-HIDDEN_SIZE: int = 128
+HIDDEN_SIZE: int = 64
 NEURAL_TICKS = 6
 LOOKAHEAD_STEPS = 12
 
@@ -27,7 +27,7 @@ MEMORY_SIZE = 100_000
 TARGET_UPDATE = 200
 
 EPSILON_START = 1.0
-EPSILON_END = 0.005
+EPSILON_END = 0.05
 EPSILON_DECAY = 0.995
 
 MAX_STEPS = 10_000
@@ -35,6 +35,6 @@ MAX_STEPS = 10_000
 MODEL_PATH = "models/snake_fly_dqn.pt"
 CHECKPOINT_PATH = "models/snake_fly_checkpoint.pt"
 
-PRINT_EVERY = 5
+PRINT_EVERY = 25
 
 SPEED_PLAY = 50
